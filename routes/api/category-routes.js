8ca112fac878060edd3b7f8ccf440a35d1bf65cc
alex.kim <alkkim@gmail.com>
2023-03-09 21:10:57 -0800

    created get by id, post put and delete routes in category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -14,7 +14,9 @@ router.get('/', (req, res) => {
       }
     ]
   })
-    .then(catData => res.json(catData))
+    .then(catData => {
+      res.json(catData)
+    })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
@@ -24,18 +26,70 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+  })
+    .then(catData => {
+      res.json(catData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(404).json('Category not found');
+    });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+    .then(catData => {
+      res.json(catData)
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(catData => {
+      res.json(catData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(catData => {
+      res.json(catData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
